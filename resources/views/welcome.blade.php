b4c0916f32b6cbfb2b12f219a6bfcd7126197028
Aasif Khan <aasifbkhan98@gmail.com>
2019-09-30 22:01:46 +0530

    ok ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
             }
         </style>
     </head>
-    <body>
+    <body style="background-image: url({{asset('images/home.jpg')}}); height: 500">
         <div class="flex-center position-ref full-height">
             @if (Route::has('login'))
                 <div class="top-right links">
@@ -82,17 +82,6 @@
             <div class="content">
                 <div class="title m-b-md">
                     <img src="{{asset('images/Msquare1.jpeg')}}">
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div>
             </div>
         </div>
